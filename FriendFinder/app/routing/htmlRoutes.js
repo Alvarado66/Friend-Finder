69fb16434ac8a3b82b82a255e18584208324e6a4
Fernando Alvarado- <Fernandoahd@icloud.com>
2020-07-01 22:49:38 -0400

    tested the routes

--- FriendFinder/app/routing/htmlRoutes.js
+++ FriendFinder/app/routing/htmlRoutes.js
@@ -6,12 +6,12 @@ module.exports = function(app) {
     // User can enter survey with URL or press Survey button.
 // Gives the user the HTML File.
 app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
 
 })
 
 // Defualt route that loads to home page.
 app.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "/app/public/home.html"));
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
 })
 }
--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -5,3 +5,12 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function(){
+    console.log("You're on port" + PORT);
+});
