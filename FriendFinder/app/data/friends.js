a9715187a070948b6f5700c4b6e785d995f158ab
Fernando Alvarado- <Fernandoahd@icloud.com>
2020-07-01 22:21:28 -0400

    added four friends

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,25 @@
+var friends = [
+    {
+        name: "Dexter",
+        photo: "https://www.pexels.com/photo/photo-of-man-wearing-grey-suit-3651814/",
+        scores: [5, 2, 4, 4, 3, 1, 3, 2, 1, 2]
+    },
+    {
+        name: "Chris",
+        photo: "https://www.pexels.com/photo/man-in-red-leather-jacket-and-black-pants-squatting-on-floor-3724358/",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+
+    },
+    {
+        name: "Leyla",
+        photo: "https://www.pexels.com/photo/woman-closing-her-eyes-3768163/",
+        scores: [5, 4, 3, 3, 4, 2, 4, 5, 4, 3]
+    },
+    {
+        name: "Darcy",
+        photo: "https://www.pexels.com/photo/woman-in-a-multicolored-swimsuit-sitting-on-beach-3722173/",
+        scores: [3, 1, 2, 4, 2, 4, 5, 2, 1, 5]
+    }
+];
+
+module.exports = friends;
