55e0aaadb65ed3e00abf8b0504767830994cfd72
Fernando Alvarado- <Fernandoahd@icloud.com>
2020-07-01 23:11:13 -0400

    added jumbotron

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -12,5 +12,5 @@ require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function(){
-    console.log("You're on port" + PORT);
+    console.log("You're on port, " + PORT);
 });
