4d41b33b56fa8f87b21ec73de4efdda187a4278c
Fernando Alvarado- <Fernandoahd@icloud.com>
2020-07-01 23:20:19 -0400

    updated html route.js

--- FriendFinder/app/routing/htmlRoutes.js
+++ FriendFinder/app/routing/htmlRoutes.js
@@ -2,16 +2,20 @@ var path = require("path");
 
 
 
-module.exports = function(app) {
+module.exports = function (app) {
     // User can enter survey with URL or press Survey button.
-// Gives the user the HTML File.
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    // Gives the user the HTML File.
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
 
-})
+    })
 
-// Defualt route that loads to home page.
-app.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/home.html"));
-})
+    // Defualt route that loads to home page.
+    // app.use(function(req, res) {
+    //     res.sendFile(path.join(__dirname, "/../public/home.html"));
+    // })
+
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "/../public/home.html"));
+    })
 }
